4fad18f0d6e54eb1c6c9a9c72cd97589cebede55
unknown <patrick.price@ASL-WIN7-JI.riverpoint.ad.wsu.edu>
2015-06-08 17:10:59 -0700

    Added initial project

---
+++ gnp_mining/gnp_mining/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gnp_mining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
